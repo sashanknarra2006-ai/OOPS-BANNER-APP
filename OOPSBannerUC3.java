96fdb488e852bf41af9d62ee9445f8a1d41e88ad
SASHANK <sashanknarra2006@gmail.com>
2026-02-24 13:32:02 +0530

    UC5: OOPS Banner App using Inline Array Initialization

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,20 @@
+public class OOPSBannerUC3 {
+    public static void main(String[] args) {
+       
+        String[] line1 = {"*", "*", "*", "*", "*", " ", "*", "*", "*", "*", "*", " ", "*", "*", "*", "*", "*", " ", "*", "*", "*", "*", "*"};
+        String[] line2 = {"*", " ", " ", " ", "*", " ", "*", " ", " ", " ", "*", " ", "*", " ", " ", " ", "*", " ", "*", " ", " ", " ", "*"};
+        String[] line3 = {"*", " ", " ", " ", "*", " ", "*", " ", " ", " ", "*", " ", "*", " ", " ", " ", "*", " ", "*", " ", " ", " ", "*"};
+        String[] line4 = {"*", " ", " ", " ", "*", " ", "*", "*", "*", "*", "*", " ", "*", "*", "*", "*", "*", " ", "*", "*", "*", "*", " "};
+        String[] line5 = {"*", " ", " ", " ", "*", " ", "*", " ", " ", " ", " ", " ", "*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "};
+        String[] line6 = {"*", " ", " ", " ", "*", " ", "*", " ", " ", " ", " ", " ", "*", " ", " ", " ", " ", " ", " ", " ", " ", " ", " "};
+        String[] line7 = {"*", "*", "*", "*", "*", " ", "*", " ", " ", " ", " ", " ", "*", " ", " ", " ", " ", " ", "*", "*", "*", "*", "*"};
+   
+        System.out.println(String.join("", line1));
+        System.out.println(String.join("", line2));
+        System.out.println(String.join("", line3));
+        System.out.println(String.join("", line4));
+        System.out.println(String.join("", line5));
+        System.out.println(String.join("", line6));
+        System.out.println(String.join("", line7));
+    }
+}
